fd8abc1097e1273f7fdb2a9934310ad20136590e
OkadaMiki <okadamiki17@gmail.com>
2024-04-02 17:17:25 +0900

    ファイル構成修正

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "../styles/index.module.css";
+
+export default function Home() {
+    return (
+        <main className={styles.main}>
+            <div>
+                <h1>aaa</h1>
+            </div>
+        </main>
+    );
+}
